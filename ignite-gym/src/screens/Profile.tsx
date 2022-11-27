7b32fa6746f45fa5f02356d83237546e499719c4
Paulo Victor <paulovictor237@gmail.com>
2022-11-27 12:15:27 -0300

    image-picker

--- ignite-gym/src/screens/Profile.tsx
+++ ignite-gym/src/screens/Profile.tsx
@@ -1,22 +1,58 @@
+import UserPhotoDefault from "@assets/userPhotoDefault.png";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
 import {
   Center,
   Heading,
   ScrollView,
   Skeleton,
   Text,
+  useToast,
   VStack,
 } from "native-base";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33;
 
 export const Profile = () => {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState("");
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        // base64: true,
+      });
+      if (photoSelected.canceled) return;
+      if (!photoSelected?.assets[0]?.uri) return;
+      const photo = photoSelected.assets[0].uri;
+      const photoInfo = await FileSystem.getInfoAsync(photo);
+      if (photoInfo.size && photoInfo.size / Math.pow(1024, 2) > 5) {
+        return toast.show({
+          title: "Essa imagem é muito grande.\nEscolha uma de até 5MB",
+          placement: "top",
+          bgColor: "red.500",
+        });
+      }
+      setUserPhoto(photoSelected.assets[0].uri);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  };
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -37,12 +73,12 @@ export const Profile = () => {
           )}
           {!photoIsLoading && (
             <UserPhoto
-              source={{ uri: "https://github.com/paulovictor237.png" }}
+              source={userPhoto ? { uri: userPhoto } : UserPhotoDefault}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
